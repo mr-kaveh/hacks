abc6d1d5213feaef361e176a12534a06e4b9627a
mr-kaveh <mr.hdavoodi@gmail.com>
2019-04-19 12:26:41 +0430

    Update 1_list_comprehensions.py
    Add Documentation Strings

--- 1_list_comprehensions.py
+++ 1_list_comprehensions.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3.6
+'''
+    Using List Comprehensions in Python
+    to Solve a Math Problem
+'''
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
     z = int(input())
     n = int(input())
-    S = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n ]
+    S = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
     print(S)
