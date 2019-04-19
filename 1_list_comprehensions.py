2733beb2f5efedb997f13cce8913fe86226edf86
mr-kaveh <mr.hdavoodi@gmail.com>
2019-04-19 12:16:30 +0430

    Add files via upload

---
+++ 1_list_comprehensions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    S = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n ]
+    print(S)
