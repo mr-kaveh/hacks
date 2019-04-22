dec8ef431abef9dbf948a980555850d50d9b3d6b
mr-kaveh <mr.hdavoodi@gmail.com>
2019-04-22 10:16:11 +0430

    6-tuples

---
+++ 6-tuples.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+'''
+    Hashing Tuple Elements
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
+    print(hash(t))
