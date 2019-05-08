a7ce723846dd97d9eb5c26f6ce14548046c59ea9
mr-kaveh <mr.hdavoodi@gmail.com>
2019-05-08 15:29:03 +0430

    mod power

---
+++ 28-modPower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+'''
+    Mod Power
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(pow(a, b))
+    print(pow(a, b, m))
+
